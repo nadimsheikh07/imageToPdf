8b7f1f1f3d17b63ec1b47fafb91aa586f5a0e21f
nadim <nadim.sheikh.07@gmail.com>
2021-01-13 18:27:10 +0530

    upload

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,13 @@ import React from "react";
 const Home = () => {
   return (
     <React.Fragment>
+
+      <form action="/api/upload" enctype="multipart/form-data" method="post">
+        <div>Text field title: <input type="text" name="title" /></div>
+        <div>File: <input type="file" name="multipleFiles" multiple="multiple" /></div>
+        <input type="submit" value="Upload" />
+      </form>
+
       <a href="/api/hello?exportPDF=true">create pdf</a>
     </React.Fragment>
   )
