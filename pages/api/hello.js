14128a104c183144e2b77fc0b9d0052e3b62392d
nadim <nadim.sheikh.07@gmail.com>
2021-01-13 17:40:34 +0530

    export to pdf

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,28 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import React from "react";
+import pdfHelper from "../../helper/pdfHelper";
 
-export default (req, res) => {
+export default async (req, res) => {
+  const { query } = req
+  const exportPDF = query.exportPDF === 'true';
+  const isServer = !!req;
+  
+  if (isServer && exportPDF) {
+    const buffer = await pdfHelper.componentToPDFBuffer(
+      <React.Fragment>
+        <h1>Hello Pdf</h1>
+      </React.Fragment>
+    );
+
+    // prompt to download pdf
+    res.setHeader('Content-disposition', 'attachment; filename="article.pdf');
+
+    // set content type
+    res.setHeader('Content-Type', 'application/pdf');
+
+    // output the pdf buffer
+    res.end(buffer);
+  }
+
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json({ message: 'export done' })
 }
