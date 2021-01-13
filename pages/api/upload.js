ae012e6c982980994a233254f3a47947106e1c5d
nadim <nadim.sheikh.07@gmail.com>
2021-01-13 23:03:47 +0530

    update image

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -7,11 +7,23 @@ export const config = {
   },
 };
 
-export default async (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.uploadDir = "./upload";
-  form.keepExtensions = true;
-  await form.parse(req, (err, fields, files) => {
-    console.log(err, fields, files);
-  });
-};
+export default (req, res) => {
+  const promise = new Promise((resolve, reject) => {
+
+    const form = new formidable.IncomingForm();
+    form.multiples = true
+    form.keepExtensions = true
+    form.uploadDir = "./public/upload"
+    form.encoding = 'utf8'
+
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      resolve({ fields, files });
+    })
+
+  })
+
+  return promise.then(({ fields, files }) => {
+    res.status(200).json({ fields, files })
+  })
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import axios from "axios";
 import React, { useState } from "react";
 import RenderHtml from "../component/renderHtml"
 const Home = () => {
   const [fileArray, setFileArray] = useState()
+  const [fileObj, setFileObj] = useState()
 
   const uploadMultipleFiles = (e) => {
     let fileObj = []
@@ -10,21 +12,27 @@ const Home = () => {
     for (let i = 0; i < fileObj[0].length; i++) {
       fileArray.push(URL.createObjectURL(fileObj[0][i]))
     }
+    setFileObj(e.target.files)
     setFileArray(fileArray)
   }
 
 
   const uploadFiles = (e) => {
     e.preventDefault()
-
-    window.location.replace(`/api/hello?files=${JSON.stringify(fileArray)}`)
+    const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+    const data = new FormData();
+    data.append('file', fileObj[0]);
+    axios.post('/api/upload', data, config).then((res) => {
+      console.log(res);
+    });
+    // window.location.replace(`/api/hello?files=${JSON.stringify(fileArray)}`)
 
   }
 
   return (
     <React.Fragment>
 
-      <form>
+      <form enctype="multipart/form-data">
         <div className="form-group multi-preview">
 
           <RenderHtml fileArray={fileArray} />
